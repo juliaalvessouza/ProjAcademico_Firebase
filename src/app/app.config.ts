f9598031f3c6a63cdf721cac463f0cf2dbb173d5
Julia Souza <julia.souza@ciag.org.br>
2025-03-13 08:42:01 -0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "projacademico-88d04", appId: "1:877499484696:web:22b8b7cd8897704b90916d", storageBucket: "projacademico-88d04.firebasestorage.app", apiKey: "AIzaSyBWo9qKqOUx3pRJn_gQcq1jIQ6m69crJiU", authDomain: "projacademico-88d04.firebaseapp.com", messagingSenderId: "877499484696" })), provideFirestore(() => getFirestore())]
 };
